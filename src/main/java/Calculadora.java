0c48b60ec53f09dcf6940ccacbbcb7ed5d96219a
thomas <t.kinderman01@ufromail.cl>
2023-05-06 18:55:07 -0400

    evidencia07

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,28 @@
+public class Calculadora {
+	private int n1;
+	private int n2;
+
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+
+	public int sumar(int n1, int n2) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int multiplicar(int n1, int n2) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Calculadora() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Calculadora(int n1, int n2) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/main/java/CarroCompra.java
@@ -0,0 +1,15 @@
+public class CarroCompra {
+	private int[][] productos;
+
+	private int calcularTotal() {
+		throw new UnsupportedOperationException();
+	}
+
+	private int subTotal(int cant, int precio) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		throw new UnsupportedOperationException();
+	}
+}
